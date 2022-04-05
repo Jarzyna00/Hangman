6ba91bb1ac807d576bf33246d8446272b9808047
Kacper Jarzynowski <kjjarzynowski@gmail.com>
2022-04-05 20:43:07 +0200

    Added onClick and stringArray in onCreate Added words array to strings.xml

--- app/src/main/java/com/sema/hangman/MainActivity.kt
+++ app/src/main/java/com/sema/hangman/MainActivity.kt
@@ -2,10 +2,27 @@ package com.sema.hangman
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.AdapterView
+import android.widget.Spinner
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
+//section Main Activity
 class MainActivity : AppCompatActivity() {
+    lateinit var stringArray: Array<String>
+
+//    section onCreate
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        stringArray = resources.getStringArray(R.array.words)
+    }
+
+    fun getRandomWord():String {
+        val number = (0..stringArray.size).random()
+        return stringArray[number]
+    }
+//    section onClick
+    fun onClick(p0: FloatingActionButton?) {
+        getRandomWord()
     }
 }
